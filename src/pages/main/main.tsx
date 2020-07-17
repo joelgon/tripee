19a331513a7d3937df3c5bffbf8dd8cc54021af4
joelgon <jl.goncalves@outlook.com.br>
2020-07-17 14:08:26 -0300

    update

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,15 +1,168 @@
-import React from 'react';
-import Menu from './menu/menu'
-import Table from './table/table'
+import React, {useState, useEffect} from 'react';
+import {makeStyles, Theme, createStyles} from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+import {
+  Table, TableBody, TableCell, TableContainer, 
+  TableHead, TableRow, TableFooter, TablePagination,
+  Paper 
+} from '@material-ui/core';
+
+import api from '../../services/api'
 
 
-const main: React.FC = () => {
+interface User {
+    id: number,
+    name: string,
+    cdcId: number,
+    ridesTotal: number,
+    priceTotal: Float32Array,
+    valAVG: Float32Array,
+    status: string
+}
+
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onChangePage: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void;
+}
+
+const useStyles1 = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexShrink: 0,
+      marginLeft: theme.spacing(2.5),
+    },
+  }),
+);
+
+export default function Teste() {
+    
+    const token = localStorage.getItem('Token')
+    const [Users, setUsers] = useState<User[]>([])
+  
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, Users.length - page * rowsPerPage);
+    const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const useStyles = makeStyles({
+        table: {
+            minWidth: 650,
+        },
+    });
+
+    const classes = useStyles()
+
+    useEffect(() => {
+        async function loaders(){
+            const response = await api.get('/admin/v1/reports/users?companyId=3', {
+            headers: { Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdhYnJpZWxkaWJhc3RpYW5pQGhvdG1haWwuY29tIiwiaWQiOjU5LCJ1c2VySWQiOjExNywiZXhwIjoxNTk5NzkzODUxLjU0NCwiaWF0IjoxNTk0NjA5ODUxfQ.OTtI51grVrmtrHMhSgGoNikNWTY9xTr1_km-zHu9W0s' }
+            })
+        setUsers(response.data.userReport)
+        }
+        loaders()
+    }, [])
+
+    function Capture(id: number){
+        console.log(id)
+    }
+
+    function TablePaginationActions(props: TablePaginationActionsProps) {
+      const classes = useStyles1();
+      const { page, rowsPerPage} = props;
+        
+      const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value -1);
+      };
+
+      const Indice = () => {
+        const TotalReg = Users.length
+        const flag = TotalReg % rowsPerPage
+        
+        if (flag === 0) {
+          return Math.floor(TotalReg / rowsPerPage) 
+        }
+        else{
+          return Math.floor(TotalReg / rowsPerPage) + 1
+        }
+      } 
+      return (
+        <div className={classes.root}>
+          
+          <Pagination 
+            count={Indice()} color='primary' page={page+1} 
+            onChange={handleChange} showFirstButton={true} 
+            showLastButton={true}size='small'siblingCount={3}/>
+          
+        </div>
+      );
+    }
+
   return (
     <div>
-      <Menu />
-      <Table />
+      <TableContainer component={Paper}>
+        <Table className={classes.table} arial-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Nome</TableCell>
+              <TableCell>CDC</TableCell>
+              <TableCell>RidesTotal</TableCell>
+              <TableCell>PriceTotal</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {(rowsPerPage > 0
+              ? Users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : Users
+            ).map((user) => (
+              <TableRow key={user.id}>
+                <TableCell component="th" scope="row" onClick={() => Capture(user.id)}>{user.id}</TableCell>
+                <TableCell component="th" onClick={() => Capture(user.id)}>{user.name}</TableCell>
+                <TableCell component="th" onClick={() => Capture(user.id)}>{user.cdcId}</TableCell>
+                <TableCell component="th" onClick={() => Capture(user.id)}>{user.ridesTotal}</TableCell>
+                <TableCell component="th" onClick={() => Capture(user.id)}>{user.priceTotal}</TableCell>
+                <TableCell component="th" onClick={() => Capture(user.id)}>{user.status}</TableCell>
+              </TableRow>
+            ))}    
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}     
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                colSpan={6}
+                count={Users.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { 'aria-label': 'rows per page' },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
-
-export default main;
