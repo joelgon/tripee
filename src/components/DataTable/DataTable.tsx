37528d93cf958421a6438b0b753e6418991ffb18
joelgon <jl.goncalves@outlook.com.br>
2020-07-20 22:53:21 -0300

    update component

---
+++ src/components/DataTable/DataTable.tsx
@@ -0,0 +1,186 @@
+import React, { useState, useMemo, useCallback } from 'react';
+
+import Pagination from "@material-ui/lab/Pagination";
+import { ArrowDropUp } from "@material-ui/icons/";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableFooter,
+  TablePagination,
+  Paper,
+} from "@material-ui/core";
+
+interface IDatatable {
+  [key: string]: any;
+}
+
+interface IDatatableColumn {
+  key: string,
+  label: string,
+}
+
+interface IDatatableOrderedColumn extends IDatatableColumn {
+  isDesc: boolean,
+}
+
+interface IDatatableProps {
+  data: IDatatable[],
+  columns: IDatatableColumn[],
+}
+
+const Datatable: React.FC<IDatatableProps> = ({ data: datatable, columns: datatableColumns }) => {
+
+  const [data, setData] = useState<IDatatable[]>(datatable);
+  const [columns, setColumns] = useState<IDatatableColumn[]>(datatableColumns);
+
+  const [orderedColumn, setOrderedColumn] = useState<IDatatableOrderedColumn | null>(null);
+
+  const [page, setPage] = useState(0);
+  const [rows, setRows] = useState(5);
+
+  const items = useMemo(() => {
+    if (rows === -1) {
+      return data;
+    }
+    
+    const skip = page * rows;
+    const take = skip + rows;
+
+    return data.slice(skip, take);
+  }, [data, page, rows]);
+
+  const isEmpty = useMemo(() => {
+    return data.length === 0
+  }, [data]);
+
+  const qtdPages = useMemo(() => {
+    if (rows === -1) {
+      return 1;
+    }
+
+    const qtd = data.length;
+
+    return Math.ceil(qtd / rows);
+  }, [data.length, rows]);
+
+  const pagePagination = useMemo(() => {
+    return page === -1 ? 1 : page + 1
+  }, [page]);
+
+  const handleChangeRowsPerPage = useCallback((qtdPages: string): void => {
+    setPage(0);
+    setRows(parseInt(qtdPages));
+  }, []);
+
+  const handlePrevNextPage = useCallback((_: any, page: number): void => {
+    setPage(page - 1);
+  }, []);
+
+  const handleOrderedColumn = useCallback(({ key, label }: IDatatableColumn): void => {
+    const isDesc = orderedColumn?.key === key ? !orderedColumn.isDesc : true;
+
+    setData(state => {
+      const orderedData = [...state]
+      orderedData.sort((a, b) => {
+        if (typeof(a[key]) === 'string') {
+          return isDesc ? ((a[key] > b[key]) ? -1 : (a[key] < b[key]) ? 1 : 0)
+            : ((a[key] < b[key]) ? -1 : (a[key] > b[key]) ? 1 : 0);
+        }
+  
+        return isDesc ? b[key] - a[key] : a[key] - b[key]
+      });
+
+      return orderedData
+    });
+
+    setOrderedColumn({
+      key,
+      label,
+      isDesc,
+    })
+
+  }, [orderedColumn]);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table arial-label="simple table">
+        <TableHead>
+          <TableRow>
+            {columns.map(col => (
+              <TableCell className="none" key={col.key}>
+                <div className="Align-items">
+                  <div>{col.label}</div>
+                  <button className="none-button"
+                    style={{ transform: `rotate(${
+                      orderedColumn !== null
+                      && orderedColumn.key === col.key 
+                      && orderedColumn.isDesc ? '180deg' : '0deg' 
+                    })`  }}
+                    onClick={() => handleOrderedColumn(col)}>
+                    <ArrowDropUp />
+                  </button>
+                </div>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          { isEmpty && (
+            <TableRow>
+              <TableCell colSpan={columns.length}>
+                Nenhum resultado encontrado!
+              </TableCell>
+            </TableRow>
+          )}
+
+          {items.map(item => (
+            <TableRow key={item[columns[0].key]} >
+              {columns.map(col => (
+                <TableCell key={col.key} component="td">
+                  {item[col.key]}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+        { !isEmpty && (
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+              colSpan={columns.length}
+              count={data.length}
+              rowsPerPage={rows}
+              page={page}
+              SelectProps={{
+                inputProps: { "aria-label": "rows per page" },
+                native: true,
+              }}
+              onChangePage={handlePrevNextPage}
+              onChangeRowsPerPage={(e) => (handleChangeRowsPerPage(e.target.value))}
+              ActionsComponent={() => (
+                <Pagination
+                  count={qtdPages}
+                  color="primary"
+                  page={pagePagination}
+                  onChange={handlePrevNextPage}
+                  showFirstButton={true}
+                  showLastButton={true}
+                  size="small"
+                  siblingCount={3}
+                />
+              )}
+            />
+          </TableRow>
+        )}
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default Datatable;
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,18 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import Pagination from "@material-ui/lab/Pagination";
-import { ArrowDropDown, ArrowDropUp } from "@material-ui/icons/";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableFooter,
-  TablePagination,
-  Paper,
-} from "@material-ui/core";
+
+import Datatable from '../../components/DataTable/DataTable'
 
 import api from "../../services/api";
 
@@ -21,254 +9,55 @@ import "./main.css";
 interface User {
   id: number;
   name: string;
-  cdcId: number;
-  ridesTotal: number;
-  priceTotal: string;
-  valAVG: Float32Array;
-  status: string;
+  cdcId?: number;
+  ridesTotal?: number;
+  priceTotal?: string;
+  valAVG?: number;
+  status?: string;
 }
-
-interface TablePaginationActionsProps {
-  count: number;
-  page: number;
-  rowsPerPage: number;
-  onChangePage: (
-    event: React.MouseEvent<HTMLButtonElement>,
-    newPage: number
-  ) => void;
-}
-
-const useStyles1 = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexShrink: 0,
-      marginLeft: theme.spacing(2.5),
-    },
-  })
-);
 
 export default function Teste() {
-  function Capture(id: number) {
-    console.log(id);
-  }
-
-  function TablePaginationActions(props: TablePaginationActionsProps) {
-    const classes = useStyles1();
-    const { page, rowsPerPage } = props;
-
-    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-      setPage(value - 1);
-    };
-
-    const Indice = () => {
-      const TotalReg = Users.length;
-      const flag = TotalReg % rowsPerPage;
-
-      if (flag === 0) {
-        return Math.floor(TotalReg / rowsPerPage);
-      } else {
-        return Math.floor(TotalReg / rowsPerPage) + 1;
-      }
-    };
-    return (
-      <div className={classes.root}>
-        <Pagination
-          count={Indice()}
-          color="primary"
-          page={page + 1}
-          onChange={handleChange}
-          showFirstButton={true}
-          showLastButton={true}
-          size="small"
-          siblingCount={3}
-        />
-      </div>
-    );
-  }
-
   const token = localStorage.getItem("Token");
-  const [Users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<User[]>([
+    { id: 1, name: 'John Doe', cdcId: 1 },
+    { id: 2, name: 'John Tree', cdcId: 10 },
+    { id: 3, name: 'John Qua', cdcId: 12 },
+    { id: 4, name: 'John Qui', cdcId: 5 },
+    { id: 5, name: 'John Sex', cdcId: 150 },
+    { id: 6, name: 'John Set', cdcId: 70 },
+    { id: 7, name: 'John Oct', cdcId: 3 },
+    { id: 8, name: 'John Non', cdcId: 0 },
+    { id: 9, name: 'John Doe', cdcId: 123 },
+    { id: 10, name: 'John Tree', cdcId: 66 },
+    { id: 11, name: 'John Qua', cdcId: 0 },
+    { id: 12, name: 'John Qui', cdcId: 102 },
+    { id: 13, name: 'John Sex', cdcId: 158 },
+    { id: 14, name: 'John Set', cdcId: 250 },
+    { id: 15, name: 'John Oct', cdcId: 78 },
+    { id: 16, name: 'John Non', cdcId: 90 },
+  ]);
 
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, Users.length - page * rowsPerPage);
-  const handleChangePage = (
-    event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number
-  ) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const useStyles = makeStyles({
-    table: {
-      minWidth: 650,
-    },
-  });
-
-  const classes = useStyles();
 
   useEffect(() => {
-    async function loaders() {
-      const response = await api.get("/admin/v1/reports/users?companyId=10", {
-        headers: {
-          Authorization:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdhYnJpZWxkaWJhc3RpYW5pQGhvdG1haWwuY29tIiwiaWQiOjU5LCJ1c2VySWQiOjExNywiZXhwIjoxNTk5NzkzODUxLjU0NCwiaWF0IjoxNTk0NjA5ODUxfQ.OTtI51grVrmtrHMhSgGoNikNWTY9xTr1_km-zHu9W0s",
-        },
-      });
-      setUsers(response.data.userReport);
-    }
-    loaders();
+    // async function loaders() {
+    //   const response = await api.get("/admin/v1/reports/players?companyId=10", {
+    //     headers: {
+    //       Authorization:
+    //         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdhYnJpZWxkaWJhc3RpYW5pQGhvdG1haWwuY29tIiwiaWQiOjU5LCJ1c2VySWQiOjExNywiZXhwIjoxNTk5NzkzODUxLjU0NCwiaWF0IjoxNTk0NjA5ODUxfQ.OTtI51grVrmtrHMhSgGoNikNWTY9xTr1_km-zHu9W0s",
+    //     },
+    //   });
+    //   setUsers(response.data.userReport);
+    // }
+    // loaders();
   }, []);
-
-  function OrderDecrById(flag: number) {
-    const user = [...Users];
-    if (flag === 0) {
-      user.sort((a, b) => b.id - a.id);
-    } else {
-      user.sort((a, b) => a.id - b.id);
-    }
-    setUsers(user);
-  }
-
-  function OrdemByName(flag: number) {
-    const user = [...Users];
-    if (flag === 0) {
-      user.sort((a, b) => (a.name > b.name ? -1 : 1));
-    } else {
-      user.sort((a, b) => (a.name < b.name ? -1 : 1));
-    }
-    setUsers(user);
-  }
-
-  function OrdemByCdc(flag: number) {
-    const user = [...Users];
-    if (flag === 0) {
-      user.sort((a, b) => (a.cdcId - b.cdcId ? -1 : 1));
-    } else {
-      user.sort((a, b) => (b.cdcId - a.cdcId ? -1 : 1));
-    }
-    setUsers(user);
-  }
-  function OrdemByPrice(flag: number) {
-    const user = [...Users];
-    user.sort(function sortFunction(a, b) {
-      var c = a.priceTotal;
-      var d = c.split(" ");
-      var e = parseFloat(d[1].replace(/\./gi, "").replace(/,/gi, "."));
-      var f = b.priceTotal;
-      var g = f.split(" ");
-      var h = parseFloat(g[1].replace(/\./gi, "").replace(/,/gi, "."));
-      if (flag === 0) {
-        return e > h ? -1 : 1;
-      } else {
-        return e < h ? -1 : 1;
-      }
-    });
-    setUsers(user);
-  }
-
-  function OrdemByCorrida(flag: number) {
-    const user = [...Users];
-    if (flag === 0) {
-      user.sort((a, b) => b.ridesTotal - a.ridesTotal);
-    } else {
-      user.sort((a, b) => a.ridesTotal - b.ridesTotal);
-    }
-    setUsers(user);
-  }
-
-  function ordination(keyname: string, flag: number){
-    const users = [...Users]
-    users.sort((a, b) => {
-      for (const key in a) {
-        if (key === keyname) {
-          console.log(a)
-        }
-        return a.id - b.id ? -1 : 1
-      }
-    });
-  }
 
   return (
     <div>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} arial-label="simple table">
-          <TableHead>
-            <TableRow>
-              {Users.length > 0 && Object.keys(Users[0]).map(keyname =>(
-                <TableCell className="none" key={keyname}>
-                  <div className="Align-items">
-                    <div>{keyname}</div>
-                    <div>
-                      <button
-                        className="none-button"
-                        onClick={() => ordination(keyname, 0)}
-                      >
-                        <ArrowDropUp />
-                      </button>
-                      <button
-                        className="none-button"
-                        onClick={() => ordination(keyname, 1)}
-                      >
-                        <ArrowDropDown />
-                      </button>
-                    </div>
-                  </div>
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {(rowsPerPage > 0
-              ? Users.slice(
-                  page * rowsPerPage,
-                  page * rowsPerPage + rowsPerPage
-                )
-              : Users
-            ).map(user => (
-              <TableRow key={user.id} >
-                {Object.entries(user).map((keyname) =>(
-                  <TableCell key={keyname[0]} component="th" onClick={() => Capture(user.id)}>
-                    {keyname[1]}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={6} />
-              </TableRow>
-            )}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                colSpan={6}
-                count={Users.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  inputProps: { "aria-label": "rows per page" },
-                  native: true,
-                }}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
+      <Datatable data={users} columns={[
+        { key: 'id', label: 'Código:' },
+        { key: 'name', label: 'Nome:' },
+        { key: 'cdcId', label: 'Teste:' }
+      ]} />
     </div>
   );
 }
