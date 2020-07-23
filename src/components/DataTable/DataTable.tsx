9761f64c8555b79f0dac6c598a03a391854c7ee9
joelgon <jl.goncalves@outlook.com.br>
2020-07-23 20:56:58 -0300

    update serach bar

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo, useCallback, useEffect } from 'react';
 
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import Pagination from "@material-ui/lab/Pagination";
-import { ArrowDropUp } from "@material-ui/icons/";
+import { ArrowDropUp, Search } from "@material-ui/icons/";
 import {
   Table,
   TableBody,
@@ -26,6 +26,12 @@ interface IDatatableColumn {
   label: string,
 }
 
+interface IColumnsValid {
+  key: string,
+  label: string,
+  valid: boolean
+}
+
 interface IDatatableOrderedColumn extends IDatatableColumn {
   isDesc: boolean,
 }
@@ -33,13 +39,18 @@ interface IDatatableOrderedColumn extends IDatatableColumn {
 interface IDatatableProps {
   data: IDatatable[],
   columns: IDatatableColumn[],
+  columnsSearch: IColumnsValid[]
+
 }
 
-const Datatable: React.FC<IDatatableProps> = ({ data: datatable, columns: datatableColumns }) => {
+const Datatable: React.FC<IDatatableProps> = ({ data: datatable, columns: datatableColumns, columnsSearch: dataColumns}) => {
 
   const [data, setData] = useState<IDatatable[]>([]);
+  
+  const [renderData, setRenderData] = useState<IDatatable[]>([])
+  const [colunmsSearch, setColunmsSearch] = useState<IColumnsValid[]>([]);
+  
   const [columns, setColumns] = useState<IDatatableColumn[]>([]);
-
   const [orderedColumn, setOrderedColumn] = useState<IDatatableOrderedColumn | null>(null);
 
   const [page, setPage] = useState(0);
@@ -47,8 +58,10 @@ const Datatable: React.FC<IDatatableProps> = ({ data: datatable, columns: datata
 
   useEffect(() => {
     setData(datatable)
+    setRenderData(data)
     setColumns(datatableColumns)
-  }, [datatable, datatableColumns]);
+    setColunmsSearch(dataColumns)
+  }, [datatable, datatableColumns, dataColumns, data]);
 
   const useStyles1 = makeStyles((theme: Theme) =>
   createStyles({
@@ -62,28 +75,28 @@ const Datatable: React.FC<IDatatableProps> = ({ data: datatable, columns: datata
 
   const items = useMemo(() => {
     if (rows === -1) {
-      return data;
+      return renderData;
     }
     
     const skip = page * rows;
     const take = skip + rows;
 
-    return data.slice(skip, take);
-  }, [data, page, rows]);
+    return renderData.slice(skip, take);
+  }, [renderData, page, rows]);
 
   const isEmpty = useMemo(() => {
-    return data.length === 0
-  }, [data]);
+    return renderData.length === 0
+  }, [renderData]);
 
   const qtdPages = useMemo(() => {
     if (rows === -1) {
       return 1;
     }
 
-    const qtd = data.length;
+    const qtd = renderData.length;
 
     return Math.ceil(qtd / rows);
-  }, [data.length, rows]);
+  }, [renderData.length, rows]);
 
   const pagePagination = useMemo(() => {
     return page === -1 ? 1 : page + 1
@@ -101,12 +114,23 @@ const Datatable: React.FC<IDatatableProps> = ({ data: datatable, columns: datata
   const handleOrderedColumn = useCallback(({ key, label }: IDatatableColumn): void => {
     const isDesc = orderedColumn?.key === key ? !orderedColumn.isDesc : true;
 
-    setData(state => {
+    setRenderData(state => {
       const orderedData = [...state]
       orderedData.sort((a, b) => {
         if (typeof(a[key]) === 'string') {
-          return isDesc ? ((a[key] > b[key]) ? -1 : (a[key] < b[key]) ? 1 : 0)
-            : ((a[key] < b[key]) ? -1 : (a[key] > b[key]) ? 1 : 0);
+          if(a[key].includes('R$')){
+            var c = a[key];
+            var d = c.split(" ");
+            var e = parseFloat(d[1].replace(/\./gi, "").replace(/,/gi, "."));
+            var f = b[key];
+            var g = f.split(" ");
+            var h = parseFloat(g[1].replace(/\./gi, "").replace(/,/gi, "."));
+            
+            return isDesc ? h - e : e - h
+          }else{
+            return isDesc ? ((a[key] > b[key]) ? -1 : (a[key] < b[key]) ? 1 : 0)
+              : ((a[key] < b[key]) ? -1 : (a[key] > b[key]) ? 1 : 0);
+          }
         }
   
         return isDesc ? b[key] - a[key] : a[key] - b[key]
@@ -127,83 +151,157 @@ const Datatable: React.FC<IDatatableProps> = ({ data: datatable, columns: datata
     console.log(item)
   };
 
-  return (
-    <TableContainer component={Paper}>
-      <Table arial-label="simple table" style={{background: '#f2f2f2'}}>
-        <TableHead>
-          <TableRow>
-            {columns.map(col => (
-              <TableCell key={col.key} >
-                <div className="Align-items">
-                  <div>{col.label}</div>
-                  <button className="none-button"
-                    style={{ transform: `rotate(${
-                      orderedColumn !== null
-                      && orderedColumn.key === col.key 
-                      && orderedColumn.isDesc ? '180deg' : '0deg' 
-                    })`  }}
-                    onClick={() => handleOrderedColumn(col)}>
-                    <ArrowDropUp />
-                  </button>
-                </div>
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          { isEmpty && (
-            <TableRow>
-              <TableCell colSpan={columns.length}>
-                Nenhum resultado encontrado!
-              </TableCell>
-            </TableRow>
-          )}
+  // Search itens
+  const handleValidation = useCallback((validator: IColumnsValid) => {
+    const col = [...colunmsSearch]
+    for (const key in col) {
+      col[key].key === validator.key ? 
+        (col[key].valid = col[key].valid ? false : true) 
+      : col[key].valid = col[key].valid ? false : false
+    }
+    setColumns(col)
+  }, [colunmsSearch])
 
-          {items.map(item => (
-            <TableRow key={item[columns[0].key]} >
+  const [search, setSearch] = useState('')
+
+  const handleSearchItens =  useCallback((flag: boolean) => {
+    const col = [...colunmsSearch]
+    for (const key in col) {
+      flag = col[key].valid ? true : (flag === true ? true : false)
+    }
+  
+    if (flag) {
+      for (const key in col) {
+        if (col[key].valid) {
+          const keyname = col[key].key
+          setRenderData(data.filter(flagItem => flagItem[keyname].toString().toLowerCase().includes(search.toString().toLowerCase())))
+        }
+      }
+    }else{
+      var objectflag = []
+      var notduplicate = true
+      for(var i=0; i< data.length; i++){
+        for (const key in data[i]) {    
+          if (data[i][key].toString().toLowerCase().indexOf(search.toLowerCase()) !== -1) {
+            notduplicate = data[i] === objectflag[objectflag.length -1] ? 
+            false : true
+            if(notduplicate){
+                objectflag.push(data[i])
+            }
+          }
+        }
+      }
+      setRenderData(objectflag)
+    }
+  },[colunmsSearch, data, search])
+
+  return (
+    <>
+      <div className='page-title'>
+        <div className='col-9'>
+            <div className='row'>
+              <ul className='options-validate'>
+                {colunmsSearch.map(col =>(
+                  <li key={col.key} className='option'>
+                    {col.label}
+                    <button className='none-button3' onClick={() => handleValidation(col)}
+                        style={{color: `${col.valid ? '#245fb3' : ''}`}}
+                    >
+                      <ArrowDropUp />
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+        </div>
+        <div className='col-3'>
+          <div className='input-icon-div'>
+            <input className='default-input icon-input m-0' 
+              value={search}
+              onChange={({target}) => setSearch(target.value) } />
+            <button className='none-button-2' onClick={() => handleSearchItens(false)}  >
+              <Search className='searh-img-icon'/>
+            </button>
+          </div>
+        </div>
+      </div>
+      <TableContainer component={Paper}>
+        <Table arial-label="simple table" style={{background: '#f2f2f2'}}>
+          <TableHead>
+            <TableRow>
               {columns.map(col => (
-                <TableCell key={col.key} component="td" onClick={() => SelectItem(item)}>
-                  {item[col.key]}
+                <TableCell key={col.key} >
+                  <div className="Align-items">
+                    <div>{col.label}</div>
+                    <button className="none-button"
+                      style={{ transform: `rotate(${
+                        orderedColumn !== null
+                        && orderedColumn.key === col.key 
+                        && orderedColumn.isDesc ? '180deg' : '0deg' 
+                      })`  }}
+                      onClick={() => handleOrderedColumn(col)}>
+                      <ArrowDropUp />
+                    </button>
+                  </div>
                 </TableCell>
               ))}
             </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-        { !isEmpty && (
-          <TableRow>
-            <TablePagination
-              style={{margin: '0px, 400px'}}
-              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-              colSpan={columns.length}
-              count={data.length}
-              rowsPerPage={rows}
-              page={page}
-              SelectProps={{
-                inputProps: { "aria-label": "rows per page" },
-                native: true,
-              }}
-              onChangePage={handlePrevNextPage}
-              onChangeRowsPerPage={(e) => (handleChangeRowsPerPage(e.target.value))}
-              ActionsComponent={() => (
-                <Pagination
-                  className={classes.root}
-                  count={qtdPages}
-                  color="primary"
-                  page={pagePagination}
-                  onChange={handlePrevNextPage}
-                  showFirstButton={true}
-                  showLastButton={true}
-                  size="small"
-                  siblingCount={3}
-                />
-              )}
-            />
-          </TableRow>
-        )}
-        </TableFooter>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            { isEmpty && (
+              <TableRow>
+                <TableCell colSpan={columns.length}>
+                  Nenhum resultado encontrado!
+                </TableCell>
+              </TableRow>
+            )}
+
+            {items.map(item => (
+              <TableRow key={item[columns[0].key]} >
+                {columns.map(col => (
+                  <TableCell key={col.key} component="td" onClick={() => SelectItem(item)}>
+                    {item[col.key]}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+          { !isEmpty && (
+            <TableRow>
+              <TablePagination
+                style={{margin: '0px, 400px'}}
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={columns.length}
+                count={renderData.length}
+                rowsPerPage={rows}
+                page={page}
+                SelectProps={{
+                  inputProps: { "aria-label": "rows per page" },
+                  native: true,
+                }}
+                onChangePage={handlePrevNextPage}
+                onChangeRowsPerPage={(e) => (handleChangeRowsPerPage(e.target.value))}
+                ActionsComponent={() => (
+                  <Pagination
+                    className={classes.root}
+                    count={qtdPages}
+                    color="primary"
+                    page={pagePagination}
+                    onChange={handlePrevNextPage}
+                    showFirstButton={true}
+                    showLastButton={true}
+                    size="small"
+                    siblingCount={3}
+                  />
+                )}
+              />
+            </TableRow>
+          )}
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </>
   )
 }
 
--- src/components/userReport/userReport.tsx
+++ src/components/userReport/userReport.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect} from "react";
 
-import DataSearch from '../dataSearch/dataSearch'
 import Datatable from '../DataTable/DataTable'
 import api from "../../services/api";
 
@@ -24,35 +23,29 @@ const UserReport: React.FC = () => {
     async function loaders() {
       const response = await api.get("/v1/reports/users?companyId=3", {
         headers: {
-          Authorization: 
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdhYnJpZWxkaWJhc3RpYW5pQGhvdG1haWwuY29tIiwiaWQiOjU5LCJ1c2VySWQiOjExNywiZXhwIjoxNTk5NzkzODUxLjU0NCwiaWF0IjoxNTk0NjA5ODUxfQ.OTtI51grVrmtrHMhSgGoNikNWTY9xTr1_km-zHu9W0s",
+          Authorization: token,
         },
       });
       setUsers(response.data.userReport);
-      setRenderUser(response.data.userReport)
     }
     loaders();
   }, []);
   
-  const [renderUser, setRenderUser] = useState<User[]>([])  
-  
   return (
-    <div>
-    <DataSearch data={users} renderData={renderUser} columns={[
-      {key: 'id', label: 'ID', valid: false},
-      {key: 'name', label: 'Nome', valid: false},
-      {key: 'cdcId', label: 'CDC', valid: false},
-    ]}/>  
-    <Datatable data={renderUser} columns={[
+    <Datatable data={users} columns={[
       { key: 'id', label: 'Código:' },
       { key: 'name', label: 'Nome:' },
       { key: 'cdcId', label: 'CDC:' },
       { key: 'ridesTotal', label: 'Corridas:'},
       { key: 'priceTotal', label: 'Valor Total:'},
       { key: 'valAVG', label: 'Valor Médio:'},
-      { key: 'status', label: 'Status:'}
-    ]} />
-    </div>
+      { key: 'status', label: 'Status:'}]} 
+      columnsSearch={[
+        {key: 'id', label: 'ID', valid: false},
+        {key: 'name', label: 'Nome', valid: false},
+        {key: 'cdcId', label: 'CDC', valid: false},
+      ]}
+    />
   );
 }
 export default UserReport
